369a0d923df70aabfa01095b459a2a9ceafe9a58
b2pwr <e54096409@gmail.com>
2020-10-15 14:44:19 +0800

    feat: try react on gh-page

---
+++ build/precache-manifest.b1d079eddfe5ca98a4a7272c3107f342.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f03696baf32be3e8ca90ffce290a53",
+    "url": "./index.html"
+  },
+  {
+    "revision": "17ff7be9fb10273a26f8",
+    "url": "./static/js/2.a65b227d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.a65b227d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2d6bdc2ace9953b2627",
+    "url": "./static/js/main.dba56efb.chunk.js"
+  },
+  {
+    "revision": "ca897b33f50d4917bba9",
+    "url": "./static/js/runtime-main.b4dab818.js"
+  }
+]);
